98c0a5c11a4524e5347d64c2be70a68a246a91d4
unknown <trahy032@gmail.com>
2023-09-01 14:28:20 +0930

    created server.js and added prompts

---
+++ server.js
@@ -0,0 +1,86 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const mysql = require('mysql2');
+const figlet = require("figlet");
+
+const PORT = process.env.PORT || 3001;
+
+// adds a banner at the start
+figlet.text(
+    "EMPLOYEE \n TRACKER",
+    {
+        horizontalLayout: "fitted",
+        verticalLayout: "fitted",
+        whitespaceBreak: true,
+    },
+    function (err, data) {
+        if (err) {
+            console.log("Figlet could not load...");
+            console.dir(err);
+            return;
+        }
+        console.log(data);
+    }
+);
+
+// array of questions for user input
+inquirer
+    .prompt([
+        {
+            type: 'list',
+            name: 'options',
+            message: 'What would you like to do?',
+            choices:
+                [
+                    'View all departments',
+                    'View all roles',
+                    'View all employees',
+                    'Add a department',
+                    'Add a role',
+                    'Add an employee',
+                    'Update employee role',
+                    'Update employee manager',
+                    'Delete department',
+                    'Delete role',
+                    'Delete employee',
+                    'Quit'
+                ],
+        }
+    ])
+    .then(result => {
+        switch (result.options) {
+            case 'View all departments':
+                viewAllDepartments();
+                break;
+            case 'View all roles':
+                viewAllRoles();
+                break;
+            case 'View all employees':
+                viewAllEmployees();
+                break;
+            case 'Add a department':
+                addDepartment();
+                break;
+            case 'Add a role':
+                addRole();
+                break;
+            case 'Add an employee':
+                addEmployee();
+                break;
+            case 'Update employee role':
+                updateEmployeeRole();
+                break;
+            case 'Update employee manager':
+                updateEmployeeManager();
+                break;
+            case 'Delete department':
+                deleteDepartment();
+                break;
+            case 'Delete role':
+                deleteRole();
+                break;
+            case 'Delete employee':
+                deleteEmployee();
+                break;
+        }
+    });
