c6796d06350629001f10408ec1ded6aca5429a82
unknown <trahy032@gmail.com>
2023-09-02 17:53:03 +0930

    added schema.sql

--- config/connection.js
+++ config/connection.js
@@ -7,9 +7,9 @@ const db = mysql.createConnection(
       host: 'localhost',
       user: 'root',
       password: process.env.PASSWORD,
-      database: 'company_db'
+      database: 'employee_db'
     },
-    console.log(`Connected to the company_db database.`)
+    console.log(`Connected to the employee_db database.`)
   );
 
 module.exports = db;
--- db/schema.sql
+++ db/schema.sql
@@ -1,21 +1,30 @@
-DROP DATABASE IF EXISTS emplyee_db;
-DROP DATABASE IF EXISTS role_db;
-DROP DATABASE IF EXISTS department_db;
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
 
-CREATE DATABASE movies_db;
+USE employee_db;
 
-USE movies_db;
-
-CREATE TABLE movies (
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  movie_name VARCHAR(100) NOT NULL
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
 );
 
-CREATE TABLE reviews (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    movie_id INT,
-    review TEXT NOT NULL,
-    FOREIGN KEY (movie_id)
-    REFERENCES movies(id)
+CREATE TABLE role (
+    id NOT NULL INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT,
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+    ON DELETE SET NULL
+);
+
+CREATE TABLE employee (
+    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    manager_id INT REFERENCES employee(id) ON DELETE SET NULL,
+    FOREIGN KEY (role_id)
+    REFERENCES role(id)
     ON DELETE SET NULL
 );
