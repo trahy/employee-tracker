25866eb30008e0f083cdd9db62ba304ed5eed1ef
unknown <trahy032@gmail.com>
2023-09-03 00:08:27 +0930

    debugging sql files

--- db/schema.sql
+++ db/schema.sql
@@ -3,28 +3,28 @@ CREATE DATABASE employee_db;
 
 USE employee_db;
 
-CREATE TABLE department (
+CREATE TABLE departments (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30) NOT NULL
 );
 
-CREATE TABLE role (
-    id NOT NULL INT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE roles (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
     department_id INT,
     FOREIGN KEY (department_id)
-    REFERENCES department(id)
+    REFERENCES departments(id)
     ON DELETE SET NULL
 );
 
-CREATE TABLE employee (
-    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+CREATE TABLE employees (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT,
-    manager_id INT REFERENCES employee(id) ON DELETE SET NULL,
+    manager_id INT NULL,
     FOREIGN KEY (role_id)
-    REFERENCES role(id)
+    REFERENCES roles(id)
     ON DELETE SET NULL
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,26 +1,33 @@
-INSERT INTO department (name)
+INSERT INTO departments (name)
 VALUES ('Engineering'),
        ('Finance'),
        ('Legal'),
        ('Sales');
 
-INSERT INTO role (department_id, title, salary)
-VALUES (1,'Sales Lead', 90000),
-       (2,'Salesperson', 70000),
-       (3,'Lead Engineer', 150000),
-       (4,'Software Engineer', 120000),
-       (5,'Account Manager', 160000),
-       (6,'Accountant', 120000),
-       (7,'Legal Team Lead', 225000),
-       (8,'Lawyer', 180000);
+SELECT * FROM departments;
 
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES
-('Gill', 'Sanders', 1, NULL),
-('Suresh', 'Adu', 3, NULL),
-('Kiara', 'Williams', 8, 5),
-('Carl', 'Young', 2, 1),
-('Vaughn', 'Simmons', 7, NULL),
-('Sophie', 'Lynn', 6, 7),
-('Caitlyn', 'Lawson', 5, NULL),
-('Keith', 'Nigh', 4, 2);
+
+INSERT INTO roles (department_id, title, salary)
+VALUES (4,'Sales Lead', 90000),
+       (4,'Salesperson', 70000),
+       (1,'Lead Engineer', 150000),
+       (1,'Software Engineer', 120000),
+       (2,'Account Manager', 160000),
+       (2,'Accountant', 120000),
+       (3,'Legal Team Lead', 225000),
+       (3,'Lawyer', 180000);
+
+SELECT * FROM roles;
+
+
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
+VALUES  ('Gill', 'Sanders', 1, NULL),
+        ('Suresh', 'Abdul', 3, NULL),
+        ('Kiara', 'Williams', 8, 5),
+        ('Carl', 'Young', 2, 1),
+        ('Vaughn', 'Simmons', 7, NULL),
+        ('Sophie', 'Lynn', 6, 7),
+        ('Caitlyn', 'Lawson', 5, NULL),
+        ('Keith', 'Nigels', 4, 2);
+
+SELECT * FROM employees;
