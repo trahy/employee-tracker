7003f026ec6e734530395575c6238fc7c41c5a09
unknown <trahy032@gmail.com>
2023-09-03 22:13:38 +0930

    created queries from prompts

--- db/seeds.sql
+++ db/seeds.sql
@@ -4,21 +4,15 @@ VALUES ('Engineering'),
        ('Legal'),
        ('Sales');
 
-SELECT * FROM departments;
-
-
-INSERT INTO roles (department_id, title, salary)
-VALUES (4,'Sales Lead', 90000),
-       (4,'Salesperson', 70000),
-       (1,'Lead Engineer', 150000),
-       (1,'Software Engineer', 120000),
-       (2,'Account Manager', 160000),
-       (2,'Accountant', 120000),
-       (3,'Legal Team Lead', 225000),
-       (3,'Lawyer', 180000);
-
-SELECT * FROM roles;
-
+INSERT INTO roles (title, salary, department_id)
+VALUES ('Sales Lead', 90000, 4),
+       ('Salesperson', 70000, 4),
+       ('Lead Engineer', 150000, 1),
+       ('Software Engineer', 120000, 1),
+       ('Account Manager', 160000, 2),
+       ('Accountant', 120000, 2),
+       ('Legal Team Lead', 225000, 3),
+       ('Lawyer', 180000, 3);
 
 INSERT INTO employees (first_name, last_name, role_id, manager_id)
 VALUES  ('Gill', 'Sanders', 1, NULL),
@@ -29,5 +23,10 @@ VALUES  ('Gill', 'Sanders', 1, NULL),
         ('Sophie', 'Lynn', 6, 7),
         ('Caitlyn', 'Lawson', 5, NULL),
         ('Keith', 'Nigels', 4, 2);
+
+
+SELECT * FROM departments;
+
+SELECT * FROM roles;
 
 SELECT * FROM employees;
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const inquirer = require('inquirer');
-const fs = require('fs');
 const figlet = require("figlet");
+const prompts = require('./lib/prompts.js')
 
 
 // adds a banner at the start
@@ -13,64 +13,4 @@ console.log(
     })
   );
 
-// array of questions for user input
-inquirer
-    .prompt([
-        {
-            type: 'list',
-            name: 'options',
-            message: 'What would you like to do?',
-            choices:
-                [
-                    'View all departments',
-                    'View all roles',
-                    'View all employees',
-                    'Add a department',
-                    'Add a role',
-                    'Add an employee',
-                    'Update employee role',
-                    'Update employee manager',
-                    'Delete department',
-                    'Delete role',
-                    'Delete employee',
-                    'Quit'
-                ],
-        }
-    ])
-    .then(result => {
-        switch (result.options) {
-            case 'View all departments':
-                viewAllDepartments();
-                break;
-            case 'View all roles':
-                viewAllRoles();
-                break;
-            case 'View all employees':
-                viewAllEmployees();
-                break;
-            case 'Add a department':
-                addDepartment();
-                break;
-            case 'Add a role':
-                addRole();
-                break;
-            case 'Add an employee':
-                addEmployee();
-                break;
-            case 'Update employee role':
-                updateEmployeeRole();
-                break;
-            case 'Update employee manager':
-                updateEmployeeManager();
-                break;
-            case 'Delete department':
-                deleteDepartment();
-                break;
-            case 'Delete role':
-                deleteRole();
-                break;
-            case 'Delete employee':
-                deleteEmployee();
-                break;
-        }
-    });
+prompts();
