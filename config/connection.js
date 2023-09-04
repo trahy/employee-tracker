57344c2ee0fc28e83c2548203b1f53cd8238e576
unknown <trahy032@gmail.com>
2023-09-04 23:27:18 +0930

    add query functions

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2');
 require('dotenv').config();
 
 // Connect to database
-const db = mysql.createConnection(
+const connection = mysql.createConnection(
     {
       host: 'localhost',
       user: 'root',
@@ -12,4 +12,4 @@ const db = mysql.createConnection(
     console.log(`Connected to the employee_db database.`)
   );
 
-module.exports = db;
+module.exports = connection;
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const inquirer = require('inquirer');
-const db = require('./config/connection.js');
+const connection = require('./config/connection.js');
 const figlet = require("figlet");
 
 // adds a banner at the start
@@ -12,31 +12,30 @@ console.log(
     })
 );
 
-// array of questions for user input
+// main menu with array of questions for user input
 function prompts() {
-    inquirer
-        .prompt([
-            {
-                type: 'list',
-                name: 'options',
-                message: 'What would you like to do?',
-                choices:
-                    [
-                        'View all departments',
-                        'View all roles',
-                        'View all employees',
-                        'Add a department',
-                        'Add a role',
-                        'Add an employee',
-                        'Update employee role',
-                        'Update employee manager',
-                        'Delete department',
-                        'Delete role',
-                        'Delete employee',
-                        'Quit'
-                    ],
-            }
-        ])
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'options',
+            message: 'What would you like to do?',
+            choices:
+                [
+                    'View all departments',
+                    'View all roles',
+                    'View all employees',
+                    'Add a department',
+                    'Add a role',
+                    'Add an employee',
+                    'Update employee role',
+                    'Update employee manager',
+                    'Delete department',
+                    'Delete role',
+                    'Delete employee',
+                    'Quit'
+                ],
+        }
+    ])
         .then(result => {
             switch (result.options) {
                 case 'View all departments':
@@ -49,19 +48,19 @@ function prompts() {
                     viewAllEmployees();
                     break;
                 case 'Add a department':
-                    addDepartment(response);
+                    addDepartment();
                     break;
                 case 'Add a role':
-                    addRole(response);
+                    addRole();
                     break;
                 case 'Add an employee':
-                    addEmployee(response);
+                    addEmployee();
                     break;
                 case 'Update employee role':
-                    updateEmployeeRole(response);
+                    updateEmployeeRole();
                     break;
                 case 'Update employee manager':
-                    updateEmployeeManager(response);
+                    updateEmployeeManager();
                     break;
                 case 'Delete department':
                     deleteDepartment();
@@ -73,45 +72,179 @@ function prompts() {
                     deleteEmployee();
                     break;
                 case 'Quit':
-                    quitPrompt();
+                    connection.end();
+                    console.log('You have exited the session')
                     break;
             }
         })
 };
 
+// functions to VIEW tables
+
 function viewAllDepartments() {
-    db.query(
-        'SELECT * FROM departments',
+    connection.query('SELECT * FROM departments',
         (err, res) => {
             if (err) throw err;
             console.table(res);
             prompts();
-        }
-    );
+        });
 };
 
 function viewAllRoles() {
-    db.query(
-        'SELECT * FROM roles',
+    connection.query('SELECT * FROM roles',
         (err, res) => {
             if (err) throw err;
             console.table(res);
             prompts();
-        }
-    );
+        });
 };
 
 function viewAllEmployees() {
-    db.query(
-        'SELECT * FROM employees',
+    connection.query('SELECT * FROM employees',
         (err, res) => {
             if (err) throw err;
             console.table(res);
             prompts();
-        }
-    );
+        });
 };
 
+// confirmation functions for ADD functions
+
+const confirmDepartmentId = function (departmentID, callback) {
+    const query = 'SELECT * FROM department WHERE id = ?';
+    connection.query(query, [departmentID], (err, res) => {
+        if (err) throw err;
+        if (res.length === 0) {
+            console.log('Department ID is invalid. Please add department.');
+            prompts();
+        } else {
+            callback();
+        }
+    });
+};
+
+const cconfirmRoleId = (roleID, callback) => {
+    const query = 'SELECT * FROM role WHERE id = ?';
+    connection.query(query, [roleID], (err, res) => {
+        if (err) throw err;
+        if (res.length === 0) {
+            console.log('Role ID is invalid. Please add role.');
+            prompts();
+        } else {
+            callback();
+        }
+    });
+};
+
+const confirmManagerID = (managerID, callback) => {
+    const query = 'SELECT * FROM employee WHERE id = ?';
+    connection.query(query, [managerID], (err, res) => {
+        if (err) throw err;
+        if (res.length === 0) {
+            console.log('Manger ID is invalid. Please ensure manager exists.');
+            prompts();
+        } else {
+            callback();
+        }
+    });
+};
+
+
+// functions to ADD to tables
+
+function addDepartment() {
+    inquirer.prompt({
+        type: 'input',
+        name: 'name',
+        message: 'Enter department name',
+    }).then((response) => {
+        connection.query('INSERT INTO departments SET ?', { name: response.name },
+            (err, res) => {
+                if (err) throw err;
+                console.log('Department created');
+                prompts();
+            });
+    });
+};
+
+function addRole() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'Enter title of role',
+        },
+        {
+            type: 'input',
+            name: 'salary',
+            message: 'Enter salary of role',
+        },
+        {
+            type: 'input',
+            name: 'department_id',
+            message: 'Enter department ID number',
+        },
+    ]).then((response) => {
+        confirmDepartmentId(response.department_id, () => {
+            connection.query('INSERT INTO role SET ?', response, (err, res) => {
+                if (err) throw err;
+                console.log('New role added');
+                prompts();
+            });
+        });
+    });
+};
+
+function addEmployee() {
+    connection.query('SELECT id, CONCAT(first_name, " ", last_name) AS name FROM employee', (err, employees) => {
+        if (err) throw err;
+
+        const managerChoices = employees.map(employee => ({ name: employee.name, value: employee.id }));
+        managerChoices.push({ name: 'None', value: null });
+
+        inquirer.prompt([
+            {
+                type: 'input',
+                name: 'first_name',
+                message: 'Enter employee first name',
+            },
+            {
+                type: 'input',
+                name: 'last_name',
+                message: 'Enter employee last name',
+            },
+            {
+                type: 'input',
+                name: 'role_id',
+                message: 'Enter role ID for employee',
+            },
+            {
+                type: 'list',
+                name: 'manager_id',
+                message: 'Enter manager for employee',
+                choices: managerChoices,
+            }
+        ]).then((answer) => {
+            confirmManagerID(answer.role_id, () => {
+                if (response.manager_id !== null) {
+                    confirmManagerID(response.manager_id, () => {
+                        connection.query('INSERT INTO employee SET ?', response, (err, res) => {
+                            if (err) throw err;
+                            console.log('Employee added');
+                            prompts();
+                        });
+                    });
+                } else {
+                    connection.query('INSERT INTO employee SET ?', response, (err, res) => {
+                        if (err) throw err;
+                        console.log('Employee added');
+                        prompts();
+                    });
+                }
+            });
+        });
+    });
+};
 
 
 
